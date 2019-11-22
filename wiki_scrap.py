a1e4bd34e457cb3a70cd3856a918e3f8a4725b06
lstacey512 <poppies.in.pink@gmail.com>
2019-11-21 19:06:09 -0600

    removed spaces from artist file

--- wiki_scrap.py
+++ wiki_scrap.py
@@ -1,28 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-import pandas
+artists = pd.read_csv('unique_artists_for_scrape.csv')
 
-#url = input('SENT FROM USER REQUEST')
-url = 'https://en.wikipedia.org/wiki/Gilbert_Stuart#Biography'
-page = requests.get(url)
+for index, row in artists.iterrows():
 
-soup = BeautifulSoup(page.text, 'html.parser')
+    first_name = artists.loc[index,'Artist_First_Name']
+    last_name = artists.loc[index,'Artist_Surname']
 
-print(soup.prettify())
+    #url = input('SENT FROM USER REQUEST')
+    url = 'https://en.wikipedia.org/wiki/'+ first_name + '_' + last_name
+    print(url)
+    #page = requests.get(url)
 
-#print artist name for header
-#header_artist_name = soup.find('h1', class_="firstHeading").text
-#print(header_artist_name)
+    #soup = BeautifulSoup(page.text, 'html.parser')
 
-#print link to image of artist
-#artist_image_link = soup.find("a",{"class":"image"})
-#print (artist_image_link["href"])
+    #print(soup.prettify())
 
-#print artist first paragraph of bio
-#artist_bio_paragraphs = (soup.find_all('p')[1].get_text())
-#print(artist_bio_paragraphs)
+    #print artist name for header
+    #header_artist_name = soup.find('h1', class_="firstHeading").text
+    #print(header_artist_name)
+
+    #print link to image of artist
+    #artist_image_link = soup.find("a",{"class":"image"})
+    #print (artist_image_link["href"])
+
+    #print artist first paragraph of bio
+    #artist_bio_paragraphs = (soup.find_all('p')[1].get_text())
+    #print(artist_bio_paragraphs)
 
 
-#table = soup.find_all('div', class_="mw-parser-output").contents
-#print(table)
+    #table = soup.find_all('div', class_="mw-parser-output").contents
+    #print(table)
