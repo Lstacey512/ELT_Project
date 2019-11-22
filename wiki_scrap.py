a5f549aca706227cf468b817a8bd5bb42850082f
jbell69 <51940579+jbell69@users.noreply.github.com>
2019-11-21 18:06:12 -0600

    Add files via upload

---
+++ wiki_scrap.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+import pandas
+
+#url = input('SENT FROM USER REQUEST')
+url = 'https://en.wikipedia.org/wiki/Vincent_van_Gogh'
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+# print(soup.prettify())
+
+#print artist name for header
+header_artist_name = soup.find('h1', class_="firstHeading").text
+print(header_artist_name)
+
+#print link to image of artist
+artist_image_link = soup.find("a",{"class":"image"})
+print (artist_image_link["href"])
+
+#print artist first paragraph of bio
+#artist_bio_paragraphs = (soup.find_all('p')[2].get_text())
+#print(artist_bio_paragraphs)
+
+
+table = soup.find_all('div', class_="mw-parser-output").contents
+print(table)
